7dc571222e783215b8bf74dc0b73edabc3d7c406
Thompson, Zachary J <zachary.thompson@moffitt.org>
2021-11-02 12:21:25 -0400

    added function name

---
+++ R/gogogadget.R
@@ -0,0 +1,10 @@
+#' GOGOGADET: A package of cool functions you wish you made.
+#'
+#' Right now it only has 4 functions but will contain more.
+#'
+#' @section Gogogogadet functions:
+#' The gogogadet functions, gogofindvar, gogogadetexport, gogostr, gogooperdir
+#'
+#' @docType package
+#' @name GOGOGADET
+NULL
